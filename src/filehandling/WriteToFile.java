a10edb4cd20589c5a6eb57243a711ada1b7f75ac
citygray <nanee9244@gamail.com>
2023-03-10 13:44:05 +0900

    WriteToFile 클래스 안에서 파일 생성

--- src/filehandling/WriteToFile.java
+++ src/filehandling/WriteToFile.java
@@ -1,15 +1,16 @@
 package filehandling;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class WriteToFile {
 
 	public static void main(String[] args) {
-
+		File myObj = new File("filename.txt"); 
 		try {
-			FileWriter myWriter = new FileWriter("filename.txt");
-			myWriter.write("Files in Java might be tricky, but it is fun enough!");
+			FileWriter myWriter = new FileWriter(myObj);
+			myWriter.write("change WriteTofile code");
 			myWriter.close();
 		} catch (IOException e) {
 			System.out.println("An error occurred.");
